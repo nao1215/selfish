362f65e143c14ddc0705a5f9170854d39df4c662
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2024-02-28 12:53:20 +0900

    Add try catch

--- lib/builtin/cd.ml
+++ lib/builtin/cd.ml
@@ -1,7 +1,10 @@
 open Printf
 
 let cd args =
-  match args with
-  | [] -> Sys.chdir (Os.home_dir())
-  | [dir] -> Sys.chdir dir
-  | _ -> printf "cd: too many arguments\n"
+  try
+    match args with
+    | [] -> Sys.chdir (Os.home_dir())
+    | [dir] -> Sys.chdir dir
+    | _ -> printf "cd: too many arguments\n"
+  with
+    | Sys_error msg -> printf "cd: %s\n" msg
