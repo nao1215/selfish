9111edd8d2bf0bab949d69d939928b74ed77d16e
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2024-03-01 07:52:00 +0900

    Check if a command exists in the PATH environment variable.

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,25 @@
 open Printf
 open Builtin
+open Unix
+
+(* Check if a command exists in PATH *)
+let exists_external_command cmd =
+  let path = Unix.getenv "PATH" in
+  let paths = String.split_on_char ':' path in
+  let rec exists_in_paths cmd = function
+    | [] -> false
+    | p :: ps ->
+        let cmd_with_path = Filename.concat p cmd in
+        if Sys.file_exists cmd_with_path then true else exists_in_paths cmd ps
+  in
+  exists_in_paths cmd paths
 
 (* Execute a command and return its exit status *)
 let execute_external_command cmd args =
-  let command = String.concat " " (cmd :: args) in
-  let exit_status = Sys.command command in
-  exit_status
+  try execvp cmd (Array.of_list (cmd :: args))
+  with Unix.Unix_error (err, _, _) ->
+    printf "Error: %s\n" (Unix.error_message err);
+    1
 
 (* Main loop of the selfish shell *)
 let rec main_loop () =
@@ -23,10 +37,12 @@ let rec main_loop () =
           if Selfish.is_builtin cmd then
             let _ = Selfish.execute_builtin cmd args in
             main_loop ()
-          else
-            (* Execute external command and save its exit status *)
+          else if exists_external_command cmd then
             let _ = execute_external_command cmd args in
             main_loop ()
+          else (
+            printf "Command not found: %s\n" cmd;
+            main_loop ())
       | [] -> main_loop ())
 
 let () = main_loop ()
